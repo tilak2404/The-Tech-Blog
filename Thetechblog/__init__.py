472a74b2f38272eb858a672fc6667c61285a93b8
tilak2404 <ctilak2404@gmail.com>
2026-01-28 20:02:33 +0530

    FIrst commit tiil Views!

---
+++ Thetechblog/__init__.py
@@ -0,0 +1,32 @@
+#techblog/__init__.py
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+app=Flask(__name__)
+
+###############DATABASE SETUP###########
+app.config['SECRET_KEY']="mysecretkey"
+basedir=os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db=SQLAlchemy(app)
+app.db=db
+Migrate(app,db)
+################# Setup COmplete###################
+
+###################LOGIN SETUP #######################
+login_manager=LoginManager()
+login_manager.init_app(app)
+login_manager.login_view='users.login'
+############### SETUP COMPLETE######################
+
+from Thetechblog.core.views import core
+app.register_blueprint(core)
+
+from Thetechblog.error_pages.handlers import error_pages
+app.register_blueprint(error_pages)
+
+
+
---
+++ Thetechblog/core/views.py
@@ -0,0 +1,12 @@
+#core/views.py
+from flask import render_template,request,url_for,redirect,Blueprint
+
+core=Blueprint('core',__name__)
+
+@core.route('/')
+def index():
+    return render_template('index.html')
+
+@core.route('/info')
+def info():
+    return render_template('info.html')
