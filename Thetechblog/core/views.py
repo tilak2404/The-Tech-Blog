9102d821dd062f38611e238ca3ae03eb80db7c70
tilak2404 <ctilak2404@gmail.com>
2026-02-02 20:24:56 +0530

    Add new threshold for recent blog posts and adjust template paddings

--- Thetechblog/core/views.py
+++ Thetechblog/core/views.py
@@ -1,6 +1,7 @@
 #core/views.py
 from Thetechblog.model import Blogpost
 from flask import render_template,request,url_for,redirect,Blueprint
+from datetime import datetime, timedelta
 
 core=Blueprint('core',__name__)
 
@@ -8,7 +9,8 @@ core=Blueprint('core',__name__)
 def index():
     page=request.args.get('page',1,type=int)
     blog_posts=Blogpost.query.order_by(Blogpost.date.desc()).paginate(page=page,per_page=5)
-    return render_template('index.html',blog_posts=blog_posts)
+    new_threshold = datetime.utcnow() - timedelta(hours=24)
+    return render_template('index.html',blog_posts=blog_posts, new_threshold=new_threshold)
 
 @core.route('/info')
 def info():
